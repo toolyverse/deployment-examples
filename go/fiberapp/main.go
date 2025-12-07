0633cb0054a57e781b8c639f457a47cc69239d8f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-12-07 13:55:38 +0300

    feat: Add initial Dockerfile, Go module, and main application for Fiber app

---
+++ go/fiberapp/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", hello)
+	log.Fatal(app.Listen(":3000"))
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World 👋!")
+}
